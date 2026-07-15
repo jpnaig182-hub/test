6a2e15132ab1e60178f84b0a5045a179a4c8dbc9
NoiSaki <jpnaig182@gmail.com>
2026-07-16 05:39:21 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -15,5 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 
+// MAKE SURE THESE TWO LINES ARE AT THE BOTTOM OF THE FILE:
+export const db = getFirestore(app);
+export const auth = getAuth(app);
