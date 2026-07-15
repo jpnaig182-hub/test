12651ac22dfff1e0e448ade0e5252f0e3ed30c45
NoiSaki <jpnaig182@gmail.com>
2026-07-16 04:37:41 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,18 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 
-// Your web app's Firebase configuration
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDcAjMKFAnNBiFfIhekOPtRadiCb0Ly8Ng",
   authDomain: "projectbahandi-7149a.firebaseapp.com",
   projectId: "projectbahandi-7149a",
   storageBucket: "projectbahandi-7149a.firebasestorage.app",
   messagingSenderId: "1017170558784",
-  appId: "1:1017170558784:web:38295ec1332f66e59304a5",
-  measurementId: "G-R0BSKY8X3D"
+  appId: "1:1017170558784:web:9d3d4a73fe01571c9304a5",
+  measurementId: "G-RGQQWTLN7D"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
